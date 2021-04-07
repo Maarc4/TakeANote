817d7747e11581b51c75e26c791d0da3b138eda3
marcs <marcserrajordidomenech@gmail.com>
2021-04-07 18:40:39 +0200

    Modificar AddNote

--- app/src/main/java/com/example/takeanote/AddNote.java
+++ app/src/main/java/com/example/takeanote/AddNote.java
@@ -1,14 +1,39 @@
 package com.example.takeanote;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class AddNote extends AppCompatActivity {
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Toast;
+
+public class AddNote extends AppCompatActivity {
+    FirebaseFirestore fStore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+        MaterialToolbar toolbar = findViewById(R.id.addNote_toolbar);
+        setSupportActionBar(toolbar);
+
+        fStore = FirebaseFirestore.getInstance();
+
+
+        //TODO: Fer que el fab sigui per escollir tipus de input o fer una bottombar
+        //TODO: posar el save button a la top bar (amb share?)
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AddNote.this, "Save btn clicked.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
