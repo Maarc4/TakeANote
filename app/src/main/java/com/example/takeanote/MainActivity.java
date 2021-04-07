20b3632cc8ef2ae48b9defb078bd13b0309b508c
marcs <marcserrajordidomenech@gmail.com>
2021-04-07 14:32:59 +0200

    Sample data per adapter (fins fer conexió amb database) i conectar adapter

--- app/src/main/java/com/example/takeanote/MainActivity.java
+++ app/src/main/java/com/example/takeanote/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.takeanote;
 
 import android.os.Bundle;
 
+import com.example.takeanote.model.Adapter;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
@@ -11,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
@@ -19,11 +21,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle toggle;
     NavigationView nav_view;
     RecyclerView listOfNotes;
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +40,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         listOfNotes = findViewById(R.id.listOfNotes);
 
-
         drawerLayout = findViewById(R.id.drawer);
         nav_view = findViewById(R.id.nav_view);
         nav_view.setNavigationItemSelectedListener(this);
@@ -43,6 +48,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout.addDrawerListener(toggle);
         toggle.setDrawerIndicatorEnabled(true);
         toggle.syncState();
+
+        // SAMPLE DATA
+        List<String> titles = new ArrayList<>();
+        List<String> content = new ArrayList<>();
+
+        titles.add("First Note");
+        content.add("First Note Content sample.");
+
+        titles.add("Second Note");
+        content.add("Second Note Content sample.");
+
+        titles.add("Third Note");
+        content.add("Third Note Content sample.");
+        // FI SAMPLE DATA
+        adapter = new Adapter(titles, content);
+        listOfNotes.setLayoutManager(new LinearLayoutManager(this));
+        listOfNotes.setAdapter(adapter);
 
 
         FloatingActionButton fab = findViewById(R.id.fab);
