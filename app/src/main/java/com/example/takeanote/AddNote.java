85cd7e9f477bf7afacc01815e860616b165bc839
marcs <marcserrajordidomenech@gmail.com>
2021-04-09 20:39:18 +0200

    posar nota a db (NO VA)

--- app/src/main/java/com/example/takeanote/AddNote.java
+++ app/src/main/java/com/example/takeanote/AddNote.java
@@ -2,19 +2,28 @@ package com.example.takeanote;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddNote extends AppCompatActivity {
-    FirebaseFirestore fStore;
+    FirebaseFirestore db;
+    EditText noteTitle, noteContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,18 +31,67 @@ public class AddNote extends AppCompatActivity {
         MaterialToolbar toolbar = findViewById(R.id.addNote_toolbar);
         setSupportActionBar(toolbar);
 
-        fStore = FirebaseFirestore.getInstance();
+        db = FirebaseFirestore.getInstance();
+        noteContent = findViewById(R.id.addNoteContent);
+        noteTitle = findViewById(R.id.addNoteTitle);
 
-
-        //TODO: Fer que el fab sigui per escollir tipus de input o fer una bottombar
+        //TODO: Fer que el fab sigui per escollir tipus de input(text,foto,audio...) o fer una bottombar
         //TODO: posar el save button a la top bar (amb share?)
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(AddNote.this, "Save btn clicked.", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(AddNote.this, "Save btn clicked.", Toast.LENGTH_SHORT).show();
+                String nTitle = noteTitle.getText().toString();
+                String nContent = noteContent.getText().toString();
+                Log.d("nTitle ->",nTitle);
+                Log.d("nContent ->",nContent);
+                if(nTitle.isEmpty() || nContent.isEmpty()){
+                    Toast.makeText(AddNote.this, "Cannot SAVE with an empty field.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //save note
+                DocumentReference docref = db.collection("notes").document();
+                Map<String, Object> note = new HashMap<>();
+                note.put("title",nTitle);
+                note.put("content",nContent);
+                Log.d("noteTitle->",note.get("title").toString());
+                Log.d("noteContent->",note.get("content").toString());
+
+                docref.set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(AddNote.this, "Note Added to database.", Toast.LENGTH_SHORT).show();
+                        onBackPressed();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(AddNote.this, "FAILED to add note to database.", Toast.LENGTH_SHORT).show();
+                        onBackPressed();
+                    }
+                });
             }
-        });
+        });/*
+                Map<String, Object> city = new HashMap<>();
+                city.put("name", "Los Angeles");
+                city.put("state", "CA");
+                city.put("country", "USA");
+
+                db.collection("cities").document("LA")
+                        .set(city)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d("ADDED ", "DocumentSnapshot successfully written!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w("FAIL", "Error writing document", e);
+                            }
+                        });*/
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
