9809696aa2f28913ec1d32d7a778b7b40cb49fe7
IvanSierra99 <11ivansm@gmail.com>
2021-06-04 10:40:09 +0200

    Projecte Final v5

--- app/src/main/java/com/example/takeanote/MainActivityViewModel.java
+++ app/src/main/java/com/example/takeanote/MainActivityViewModel.java
@@ -281,7 +281,7 @@ public class MainActivityViewModel extends AndroidViewModel {
                         .addOnSuccessListener(documentSnapshot -> {
                             storageReference.child(documentSnapshot.getString("url")).delete();
                             notesData.setValue(newData);
-                            DocumentReference docRef1 = db.collection("notes").document(user.getUid()).collection("paintNotes").document(iinfo.getId());
+                            DocumentReference docRef1 = db.collection("notes").document(user.getUid()).collection("imageNotes").document(iinfo.getId());
                             docRef1.delete().addOnSuccessListener(aVoid -> {
                                 Toast.makeText(getApplication().getApplicationContext(), getApplication().getResources().getString(R.string.toast_paint_deleted), Toast.LENGTH_SHORT).show();
                                 notesData.setValue(newData);
